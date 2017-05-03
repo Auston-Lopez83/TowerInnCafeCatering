be1bc21b1f4b64526fa333320afb3e4638da9411
Auston Lopez <Zoecoco73@Austons-iMac.local>
2017-05-03 13:21:14 -0500

    More updates
    Added more items to Package Deal

--- TowerInnCafeCatering/Models/PackageDealRepository.cs
+++ TowerInnCafeCatering/Models/PackageDealRepository.cs
@@ -7,10 +7,11 @@ namespace TowerInnCafeCatering.Models
     {
         public List<PackageDeal> PackageDeals => new List<PackageDeal>
         {
-            new PackageDeal { Name = "Chicken and Pasta", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = ""},
-            new PackageDeal { Name = "Chicken and Beef", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = ""},
-            new PackageDeal { Name = "Chicken and Fish", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = ""},
-
+            new PackageDeal { Name = "Chicken and Pasta", Description = "Choice one of Ritz Dijon Chicken, BBQ Chicken, or Chicken Piccata, and your choice of two Pastas with Sauce, and Fresh Baked Breadsticks.", Price = 10M, Image = "~/images/banner1.jpg"},
+            new PackageDeal { Name = "Tower Inn’s Grand Buffet Package", Description = "Roast Beef, Ritz Dijon Chicken, Sun-dried Tomato Cavatappi Pasta, Rosemary Roasted Potatoes, Vegetable Medley, and Fresh Baked Bread.", Price = 19m, Image = "~/images/banner2.jpg"},
+            new PackageDeal { Name = "Pasta Package", Description = "Your Choice of Three Pastas, Roasted Vegetables and Fresh Baked Breadsticks.", Price = 10M, Image = "~/images/banner3.jpg"},
+            new PackageDeal { Name = "Breakfast Package", Description = "Spinach & Feta Quiche, Ham & Cheddar Quiche, American Potatoes, Pancakes, Bacon & Ham, and served with Fruit", Price = 10M,Image =""},
+            new PackageDeal { Name = "Pizza and Pasta Buffet", Description = "Your choice of Gourmet Pizzas, Pastas with Sauce, Fresh Baked Breadsticks, and Cinnamon Sugar Twists.", Price = 9M,Image =""},
         };
     }
 }
