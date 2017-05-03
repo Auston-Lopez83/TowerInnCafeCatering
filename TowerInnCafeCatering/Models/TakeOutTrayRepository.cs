4db8a2235f064ef42f588482fc24a376b946611a
Auston Lopez <Zoecoco73@Austons-iMac.local>
2017-05-03 13:54:34 -0500

    Update

--- TowerInnCafeCatering/Models/TakeOutTrayRepository.cs
+++ TowerInnCafeCatering/Models/TakeOutTrayRepository.cs
@@ -7,7 +7,7 @@ namespace TowerInnCafeCatering.Models
     {
         public List<TakeOutTray> TakeOutTrays => new List<TakeOutTray>
         {
-            new TakeOutTray { Name = "Beef and Pasta", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = ""},
+            new TakeOutTray { Name = "Cheese & Crackers", Description = "Assorment of Cheeses and Gourmet Crackers", Price = 10M, Image = ""},
             new TakeOutTray { Name = "Fish and Beef", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = ""},
             new TakeOutTray { Name = "Beef and Fish", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = ""},
 
